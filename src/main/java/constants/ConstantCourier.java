f14b10a64ea69d33fc0bbd07286a18958c291ba9
Chizhov Vitaliy <chizhov.vitaliy@inbox.ru>
2025-07-14 07:21:45 +0300

    constants

---
+++ src/main/java/constants/ConstantCourier.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class ConstantCourier {
+    public static final String NAME ="Ivan_TEST";
+    public static final String LOGIN ="Login_TEST";
+    public static final String PASSWORD ="password_TEST";
+}
