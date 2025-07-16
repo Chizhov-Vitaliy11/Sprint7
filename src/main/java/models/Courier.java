05bf9677ccfc439080a53be7c52c7a58f5bf1aa3
Chizhov Vitaliy <chizhov.vitaliy@inbox.ru>
2025-07-16 23:38:57 +0300

    fix models

--- src/main/java/models/Courier.java
+++ src/main/java/models/Courier.java
@@ -1,32 +1,20 @@
 package models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Courier {
 
-    private String name;
-    private String login;
-    private String password;
 
-    public Courier(String login, String password, String name) {
-        this.login = login;
-        this.password = password;
-        this.name = name;
-    }
+    private  String login;
+    private  String password;
+    private  String name;
 
     public Courier(String login, String password) {
         this.login = login;
         this.password = password;
-
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public String getName() {
-        return name;
-    }
 }
--- src/main/java/models/Order.java
+++ src/main/java/models/Order.java
@@ -2,6 +2,11 @@ package models;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Order {
     private String firstName;
     private String lastName;
@@ -13,52 +18,6 @@ public class Order {
     private String comment;
     private List<String> color;
 
-    public Order(String firstName, String lastName, String address, String metroStation, String phone,
-                 Number rentTime, String deliveryDate, String comment, List<String> color) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.metroStation = metroStation;
-        this.phone = phone;
-        this.rentTime = rentTime;
-        this.deliveryDate = deliveryDate;
-        this.comment = comment;
-        this.color = color;
-    }
 
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public String getMetroStation() {
-        return metroStation;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public Number getRentTime() {
-        return rentTime;
-    }
-
-    public String getDeliveryDate() {
-        return deliveryDate;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public List<String> getColor() {
-        return color;
-    }
 }
