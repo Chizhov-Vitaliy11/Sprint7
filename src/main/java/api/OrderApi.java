bb396b9f3d204f3c7ef34c5fc8a91db2d406782c
Chizhov Vitaliy <chizhov.vitaliy@inbox.ru>
2025-07-16 23:37:25 +0300

    add api constants

--- src/main/java/api/OrderApi.java
+++ src/main/java/api/OrderApi.java
@@ -1,4 +1,9 @@
 package api;
 
 public class OrderApi {
+    public static final String API_CREATE_ORDER ="/api/v1/orders";
+    public static final String API_GET_ORDERS ="/api/v1/orders";
+    public static final String API_CANCEL_ORDER ="/api/v1/orders/cancel?track=";
+    public static final String API_SEARCH_ORDER ="/api/v1/orders/track?t=";
+
 }
